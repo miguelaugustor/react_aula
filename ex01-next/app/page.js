e0fc52c51aebe81ee2ad695f1953b0e8dff01832
Miguel Augusto <migguelaugustolima@gmail.com>
2025-08-15 00:23:00 +0000

    Hello world 2 - React

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -1,10 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+//import Image from "next/image";
+//import styles from "./page.module.css";
+
+import { Profile } from "@/components/Profile"
 
 export default function Home() {
   return (
     <div >
-      ooi
+      <h1>Olá Mundo!</h1>
+      <Profile/>
     </div>
   );
 }
---
+++ ex01-next/components/components/Profile.js
@@ -0,0 +1,8 @@
+export function Profile() {
+  return (
+    <img
+      scr="https://i0.wp.com/www.iconvsicon.com/wp-content/uploads/2025/02/Beyonce-Cowboy-Carter-Tour.png?resize=800%2C1000&ssl=1"
+      alt="BEYONCÉ - COWBOY CARTER"
+    />
+  );
+}
